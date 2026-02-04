45558524dafb964dada5d9f22f6cbac24def8856
Samuel Nóbrega <samuelnobrega902@gmail.com>
2026-02-04 12:29:52 -0300

    feat: add more tests to ConstantsTest

--- src/test/java/dev/toonformat/jtoon/util/ConstantsTest.java
+++ src/test/java/dev/toonformat/jtoon/util/ConstantsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
+import static dev.toonformat.jtoon.util.Constants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -30,5 +31,85 @@ public class ConstantsTest {
         final Throwable cause = thrown.getCause();
         assertInstanceOf(UnsupportedOperationException.class, cause);
         assertEquals("Utility class cannot be instantiated", cause.getMessage());
+    }
+
+    @Test
+    void expectsListItemMarker() {
+        assertEquals("-", LIST_ITEM_MARKER);
+    }
+
+    @Test
+    void expectsListItemPrefix() {
+        assertEquals("- ", LIST_ITEM_PREFIX);
+    }
+
+    @Test
+    void expectsComma() {
+        assertEquals(",", COMMA);
+    }
+
+    @Test
+    void expectsColon() {
+        assertEquals(":", COLON);
+    }
+
+    @Test
+    void expectsSpace() {
+        assertEquals(" ", SPACE);
+    }
+
+    @Test
+    void expectsDot() {
+        assertEquals(".", DOT);
+    }
+
+    @Test
+    void expectsHashtag() {
+        assertEquals("#", HASHTAG);
+    }
+
+    @Test
+    void expectsOpenBracket() {
+        assertEquals("[", OPEN_BRACKET);
+    }
+
+    @Test
+    void expectsCloseBracket() {
+        assertEquals("]", CLOSE_BRACKET);
+    }
+
+    @Test
+    void expectsOpenBrace() {
+        assertEquals("{", OPEN_BRACE);
+    }
+
+    @Test
+    void expectsCloseBrace() {
+        assertEquals("}", CLOSE_BRACE);
+    }
+
+    @Test
+    void expectsNullLiteral() {
+        assertEquals("null", NULL_LITERAL);
+    }
+
+    @Test
+    void expectsTrueLiteral() {
+        assertEquals("true", TRUE_LITERAL);
+    }
+
+    @Test
+    void expectsFalseLiteral() {
+        assertEquals("false", FALSE_LITERAL);
+    }
+
+    @Test
+    void expectsBackslash() {
+        assertEquals('\\', BACKSLASH);
+    }
+
+    @Test
+    void expectsDoubleQuote() {
+        assertEquals('"', DOUBLE_QUOTE);
     }
 }
